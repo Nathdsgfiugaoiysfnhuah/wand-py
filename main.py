e7197f7c5c342a3382255e79ba9082cbff6795ef
Nahtan <nathan.titirangi@gmail.com>
2023-07-31 22:43:15 +1200

    fix scaling and add highlight

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import d_types
 import d_map
 import time
 
-WIDTH = 34*26 + 2
+WIDTH = 42*26 + 2
 HEIGHT = 1080//2
-FPS = 60
+FPS = 240
 
 # Define Colors
 WHITE = (255, 255, 255)
@@ -27,7 +27,9 @@ textinput = pygame_textinput.TextInputVisualizer(font_color=pygame.Color(
     (255, 255, 255)), cursor_color=pygame.Color((255, 255, 255), cursor_width=20))
 
 box = pygame.transform.scale(pygame.image.load(
-    "ui/inventory_box.png").convert_alpha(), (32, 32))
+    "ui/full_inventory_box.png").convert_alpha(), (40, 40))
+highlight = pygame.transform.scale(pygame.image.load(
+    "ui/item_bg_purchase_1.png").convert_alpha(), (40, 40))
 spell_info = []
 translations = d_map.get_info()
 enum = ["projectile", "static_projectile", "modifier",
@@ -67,23 +69,23 @@ def tick():
 
 
 spells = []
+sel = 0
 
 
 def render():
     x = 0
     y = 2
     for i in range(26):
-        screen.blit(box, (34*i+2, 0))
-
+        screen.blit(box, (42*i+2, 2))
+    screen.blit(highlight, (42*sel+2, 2))
     for k, spell_v in enumerate(spells):
-        screen.blit(spell_info[name_to_id(spell_v)][2], (34*k+2, 0))
+        screen.blit(spell_info[name_to_id(spell_v)][2], (42*k+2+4, 2+4))
 
-    screen.blit(textinput.surface, (2, 32))
+    screen.blit(textinput.surface, (2, 40))
     for k, spell_v in enumerate(spell_info):
         if textinput.value.lower() in spell_v[0].lower():
-            # if y <= (540//34):
-            screen.blit(box, (x*34+2, y*34))
-            screen.blit(spell_info[k][2], (x*34+2, y*34+2))
+            screen.blit(box, (x*42+2, y*42+2))
+            screen.blit(spell_info[k][2], (x*42+2+4, y*42+2+4))
             x += 1
             if x >= 26:
                 y += 1
@@ -97,27 +99,37 @@ while running:
     clock.tick(FPS)
     events = pygame.event.get()
 
-    textinput.update(events)
-
     for event in events:
         # listening for the the X button at the top
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            pos = ((pos[0]-2)//34, pos[1]//34)  # 2 is spell gui, 1
-            if pos[1] >= 2:
-                spell = list(filter(lambda spell: textinput.value.lower(
-                ) in spell[0].lower(), spell_info))[(pos[1]-2)*26+pos[0]]
-                # print(spell[3])
-                spells.append(spell[3])
-            elif pos[1] == 0:
-                try:
-                    spells.pop(pos[0])
-                except IndexError:
-                    pass
+            if event.button == 1:
+                pos = pygame.mouse.get_pos()
+                pos = ((pos[0]-2)//42, pos[1]//42)  # 2 is spell gui, 1
+                if pos[1] >= 2:
+                    spell = list(filter(lambda spell: textinput.value.lower(
+                    ) in spell[0].lower(), spell_info))[(pos[1]-2)*26+pos[0]]
+                    # print(spell[3])
+                    spells.insert(sel, spell[3])
+                    sel += 1
+                elif pos[1] == 0:
+                    try:
+                        spells.pop(pos[0])
+                        if pos[0] <= sel:
+                            sel -= 1
+                    except IndexError:
+                        pass
+            elif event.button == 5:
+                sel += 1
+                sel = min(26, sel)
+            elif event.button == 4:
+                sel -= 1
+                sel = max(0, sel)
             # print(pos)
+
+    textinput.update(events)
 
     # 3 Draw/render
     screen.fill(BLACK)
