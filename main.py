ea55e1c0c1e58f66118ebd9274538698bed7ca85
Dice Toss <dicetoss@pengiun>
2023-07-31 15:01:08 +1200

    revamped stuff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 import random
 import os
+import types
+import map
+import time
 
 WIDTH = 32*26
 HEIGHT = 1080/2
@@ -18,29 +21,45 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Wand Editor")
 clock = pygame.time.Clock()     ## For syncing the FPS
-
 box = pygame.transform.scale(pygame.image.load("ui/inventory_box.png").convert_alpha(),(32,32))
-spell_gfx = []
-spell_ids = {}
-i = 0
+spell_info = []
+translations = map.get_info()
+enum = ["projectile","static_projectile","modifier","draw_many","material","other","utility","passive"]
+for spell in types.get_info():
+  #print(spell[0][1:])
+  try:
+    spell_info.append((
+      translations[spell[0][1:]],
+      enum[spell[1]],
+      pygame.transform.scale(pygame.image.load("spells/"+spell[2].split("/")[-1]).convert_alpha(),(32,32)),
+      spell[3]
+    ))
+  except:
+    spell_info.append((
+      "???",
+      enum[spell[1]],
+      pygame.transform.scale(pygame.image.load("spells/"+spell[2].split("/")[-1]).convert_alpha(),(32,32)),
+      spell[3]
+    ))
+    print(spell[0][1:])
 
-for file_name in os.listdir("spells"):
-  spell_gfx.append(pygame.transform.scale(pygame.image.load("spells/"+file_name).convert_alpha(),(32,32)))
-  spell_ids[file_name.split(".p")[0]] = i
-  i += 1
+def name_to_id(name):
+  for k,v in enumerate(spell_info):
+    if v[3] == name:
+      return k
 
 ## group all the sprites together for ease of update
 def tick():
   pass
 
-spells = ["damage"]
+spells = ["DAMAGE","LUMINOUS_DRILL"]
 
 def render():
   for i in range(26):
     screen.blit(box,(32*i,0))
   
   for k,spell in enumerate(spells):
-    screen.blit(spell_gfx[spell_ids[spell]],(32*k,0))
+    screen.blit(spell_info[name_to_id(spell)][2],(32*k,0))
 
 ## Game loop
 running = True
